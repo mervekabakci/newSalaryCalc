e5ac0dc50d994a4b552c1fc82aaea5da4b4f2854
TriosMerve <merve@trios.com.tr>
2025-06-02 18:21:39 +0300

    prompt ile kullanıcadan alınan maas ve oranı hesaplanarak yenı maas console da yazdırılır.

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+
+let userSalary = 0;
+let userSalaryPercent = 0;
+let newSalaryCalc = 0;
+let newUserSalary = 0;
+
+function getSalary(){
+    userSalary = Number(prompt("Lütfen maaşınızı giriniz."));
+    userSalaryPercent = Number(prompt("Lütfen maaş zamn oranınızı giriniz."));
+    return newSalaryCalc = userSalaryPercent / 100; //buradan donen deger ıle ilk basta tanımladıgımız 0 olan degerler guncellenır
+}
+function newSalary() {
+    getSalary();
+    newUserSalary = userSalary * (1 + newSalaryCalc);
+    console.log(`Aldığınız Maaş : ${userSalary} - Aldığınız zam oranı : ${userSalaryPercent}
+Güncel Maaşınız : ${newUserSalary}`);   
+}
+
+newSalary();
