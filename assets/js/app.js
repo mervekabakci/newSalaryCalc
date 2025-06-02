cd38df71b327aa19406c8cef1b926055e425e80b
TriosMerve <merve@trios.com.tr>
2025-06-02 18:29:21 +0300

    fonksıyonları fath arrow ıle guncelleme

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,12 @@ let userSalaryPercent = 0;
 let newSalaryCalc = 0;
 let newUserSalary = 0;
 
-function getSalary(){
+getSalary = () =>{
     userSalary = Number(prompt("Lütfen maaşınızı giriniz."));
     userSalaryPercent = Number(prompt("Lütfen maaş zamn oranınızı giriniz."));
     return newSalaryCalc = userSalaryPercent / 100; //buradan donen deger ıle ilk basta tanımladıgımız 0 olan degerler guncellenır
 }
-function newSalary() {
+newSalary = () => {
     getSalary();
     newUserSalary = userSalary * (1 + newSalaryCalc);
     console.log(`Aldığınız Maaş : ${userSalary} - Aldığınız zam oranı : ${userSalaryPercent}
